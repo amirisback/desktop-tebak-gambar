7d978f0c03bad9788d102902759591abbcce6f42
amirisback <f.miir117@gmail.com>
2017-12-07 15:19:37 +0700

    Version 1.0

---
+++ src/tebakgambar/TebakGambar.java
@@ -0,0 +1,26 @@
+/*
+ * Muhammad Faisal Amir
+ * f.miir117@gmail.com
+ * id.amirisback.bandung
+ * Copyright 2017
+ */
+package tebakgambar;
+
+/**
+ *
+ * @author Faisal Amir
+ */
+public class TebakGambar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        start st = new start();
+        st.setVisible(true);
+        
+        
+    }
+    
+}
